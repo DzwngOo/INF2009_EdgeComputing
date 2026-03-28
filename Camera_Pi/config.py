efc0a022042396edec1901af174e1b4994120464
DericM-hub <132824207+DericM-hub@users.noreply.github.com>
2026-03-29 03:34:02 +0800

    Copied CameraPi changes from fallBack branch

--- Camera_Pi/config.py
+++ Camera_Pi/config.py
@@ -9,9 +9,10 @@ PROJECT_DIR = BASE_DIR.parent
 ##### CAMERA PI #####
 # Model
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-#MODEL = os.path.join(PROJECT_ROOT, "yolo26n.pt")
 MODEL = str(PROJECT_DIR / "yolo11s_ncnn_model") # applied quantization on backend
 DEVICE = "cpu"           # Pi default
+# Camera Config
+CAMERA_ID = "cam1"
 # Input
 SOURCE = 0               # webcam index (e.g., /dev/video2)
 IMGSZ = 320              # 320/416 for better FPS on Pi
